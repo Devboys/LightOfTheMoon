7254d62bd1824fc12c9b2d764cbfb38ad3dc29f6
Jeppe Faber <JeppestougaardFaber@gmail.com>
2019-12-11 15:31:05 +0100

    Asset Manager now also loads sprite atlases

--- DefaultAssetManager.hpp
+++ DefaultAssetManager.hpp
@@ -2,7 +2,7 @@
  * Created by Jeppe Faber on 05/12/2019.
  * LIST OF EDITS (reverse chronological order - add last on top):
  * +
- * + Jeppe Faber [07/12/19] - Basic implementation - default asset manager - should return nothing or default values
+ * + Jeppe Faber [07/12/19] - Basic implementation - default asset manager - Returns 
  */
 
 #pragma once
@@ -14,10 +14,11 @@ public:
 	virtual void loadAssets();
 	virtual rapidjson::Document* getJsonAsset(std::string filename);
 	virtual Mix_Chunk* getSoundAsset(std::string filename);
-	virtual void getSpriteAsset();
+	virtual std::shared_ptr<sre::SpriteAtlas> getSpriteAtlas(std::string jsonPath);
 
 private:
 	rapidjson::Document* defaultJson;
 	Mix_Chunk* defaultSound;
+	std::shared_ptr<sre::SpriteAtlas> defaultAtlas;
 
 };
