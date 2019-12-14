3c606fb019b4418a1025832a0ab441a58c240b00
Jeppe Faber <JeppestougaardFaber@gmail.com>
2019-12-14 12:35:42 +0100

    cleaned up a left-over comment in GameAudio.cpp

--- GameAudio.cpp
+++ GameAudio.cpp
@@ -27,10 +27,6 @@ void GameAudio::playOneshot(std::string filename) {
 	
 	Mix_Chunk* sound = AssetLocator::getService()->getSoundAsset(filename);
 
-	/*if (sound == NULL) {
-		loadSound(filename);
-		sound = sounds.at(filename);
-	}*/
 	//play cached sound at next free mixer channel
 	Mix_PlayChannel(-1, sound, 0);
 
